4e62a18dd371ec6f15789c2174143724922c590e
abdulzaheradnan <abdulzaheradnan143@gmail.com>
2023-11-22 00:07:45 +0530

    js
    hello

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app=express()
 
 const PORT=3000
 app.get("/",(req,res)=>{
-    res.send(`<h1>hello bro kya haal hai</h1>`)
+    res.send(`<h1>hello bro kya haal hai mai addu bolrau</h1>`)
 
 })
 app.listen(PORT,()=>{
